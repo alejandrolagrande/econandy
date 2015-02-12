8f1f4f675dbec691537a5276dcd0f4e0ab3a5a3e
Grant Alexander <grant.fricano@gmail.com>
2015-02-12 19:42:15 +0000

    cv stuff and layout

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def cv
+    send_file "#{Rails.root}/public/andrew-h-mccallum-cv.pdf", 
+            :type => "application/pdf", 
+            :disposition => 'inline'
+  end
 end
